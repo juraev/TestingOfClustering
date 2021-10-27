a2cbd10f2ff30d91ac75494416d04c57fec9cf35
juraev <kh.j.mx@mail.ru>
2021-10-27 21:00:50 +0500

    Implemented: k = 1 for radius and diameter with L2

--- cmake-build-debug/CMakeFiles/TestingOfClustering.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/TestingOfClustering.dir/DependInfo.cmake
@@ -8,8 +8,16 @@ set(CMAKE_DEPENDS_CHECK_CXX
   )
 set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BOOST_ALL_NO_LIB"
+  "CGAL_USE_GMPXX=1"
+  )
+
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "/usr/local/opt/cgal/include"
   )
 
 # Targets to which this target links.
--- main.cpp
+++ main.cpp
@@ -6,10 +6,20 @@ using std::vector, std::cout;
 
 int main() {
 
-    SamplingAlgorithm<Cost::RADIUS, Metric::L2, K::ANY> samplingAlgorithm{0.5f, 4.0f};
 
+    SamplingAlgorithm<Cost::RADIUS, Metric::L2, K::ONE> samplingAlgorithm{0.5f, 4.0f};
 
-    samplingAlgorithm.isClusterable(vector<clustering::Point>(), NULL);
+    Dist dist_l2 = [](const Point& a, const Point& b, int d){
+        double res = 0;
+        for(int i = 0; i < d; i ++){
+            res += (a[i] - b[i]) * (a[i] - b[i]);
+        }
+        return std::sqrt(res);
+    };
+
+    auto dataset = vector<clustering::Point>{{1}, {2}, {3}, {9.01}};
+
+    cout << samplingAlgorithm.isClusterable(0.5, 1, dataset, dist_l2);
 
     return 0;
 }
